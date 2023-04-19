9d6fa562682cdf551bdfbbcf57e54d3d8233f232
Devilking6105 <43361618+MichaelHDesigns@users.noreply.github.com>
2023-04-19 01:27:52 -0400

    Update pinata.js

--- src/pinata.js
+++ src/pinata.js
@@ -12,6 +12,33 @@ const opts = {
 };
 const rateLimiter = new RateLimiterMemory(opts);
 
+export const uploadJSONToIPFS = async(JSONBody) => {
+    const url = `https://api.pinata.cloud/pinning/pinJSONToIPFS`;
+    //making axios POST request to Pinata ⬇️
+    return axios 
+        .post(url, JSONBody, {
+            headers: {
+                pinata_api_key: key,
+                pinata_secret_api_key: secret,
+                'Access-Control-Allow-Origin': '*'
+            }
+        })
+        .then(function (response) {
+           return {
+               success: true,
+               pinataURL: "https://gateway.pinata.cloud/ipfs/" + response.data.IpfsHash
+           };
+        })
+        .catch(function (error) {
+            console.log(error)
+            return {
+                success: false,
+                message: error.message,
+            }
+
+    });
+};
+
 export const uploadFileToIPFS = async(file) => {
     const url = `https://api.pinata.cloud/pinning/pinFileToIPFS`;
 
